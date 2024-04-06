397c4bd7ff9fd34fbfd0498c63c21a283789064d
jacoblilje <162449979+jacoblilje@users.noreply.github.com>
2024-04-06 08:28:40 +0200

    LHD_scrape.py

--- LHD_scrape.py
+++ LHD_scrape.py
@@ -14,6 +14,7 @@ import os
 import streamlit as st
 from urllib.parse import urljoin
 import time
+from openpyxl import Workbook
 #%%
 def scrape(url, key_word, flag):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
