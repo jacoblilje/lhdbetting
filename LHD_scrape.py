4cad69d999dd04ebf5295b0f677ea0468efabc42
jacoblilje <162449979+jacoblilje@users.noreply.github.com>
2024-04-06 08:43:54 +0200

    LHD_scrape.py

--- LHD_scrape.py
+++ LHD_scrape.py
@@ -18,7 +18,7 @@ from openpyxl import Workbook
 
 #%%
 st.title("lhdbetting scraping app")
-st.markdown(hide_st_style, unsafe_allow_html=True)
+
 def scrape(url, key_word, flag):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     page = requests.get(url, headers=headers)
